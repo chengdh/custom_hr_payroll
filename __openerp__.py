0ff22e0f6e0fd53d02e04114e3c0ebe106d82974
chengdh <cheng.donghui@gmail.com>
2013-06-23 17:33:52 +0800

    first commit

---
+++ __openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'custom hr payroll',
+    'version': '0.1',
+    'category': 'Human Resources',
+    'description': """定制工资表-for ktv""",
+    'author': 'chengdh (cheng.donghui@gmail.com)',
+    'website': '',
+    'license': 'AGPL-3',
+    'depends': ['hr_payroll', 'web'],
+    'init_xml': [],
+    'update_xml': [],
+    'demo_xml': [],
+    'active': False,
+    'installable': True,
+    'web':True,
+    'css': [
+        'static/css/custom_hr_payroll.css',
+    ],
+    'js': [
+        'static/js/custom_hr_payroll.js',
+    ],
+    'xml': [
+        'static/xml/custom_hr_payroll.xml',
+    ],
+}
+
---
+++ static/js/custom_hr_payroll.js
@@ -0,0 +1,5 @@
+//  @@@ custom_hr_payroll custom JS @@@
+
+openerp.{normalized_name} = function(openerp) {
+    // extend openerp here
+}
